f4e76d7e010e05f99920a692d9cc630979d3e1cb
GHOST <ghostdevbusiness@gmail.com>
2023-10-01 02:25:53 +0100

    feat: overlay

--- packages/obra-icons/src/routes/icons/+page.svelte
+++ packages/obra-icons/src/routes/icons/+page.svelte
@@ -62,9 +62,13 @@
 						{@const nameKebab = iconNameKebab(name)}
 
 						<li class="icon-item">
-							<!-- Want to render the icon here e.g. <svelte:component this={icon} />-->
-							<!-- {transformIconString(icon)} -->
 							<svelte:component this={getIconComponent(name)} />
+
+							<div class="overlay">
+								<button class="button"> Download SVG </button>
+								<button class="button"> Copy SVG </button>
+								<button class="button"> Copy import </button>
+							</div>
 						</li>
 					{/each}
 				</ul>
@@ -72,3 +76,76 @@
 		</div>
 	</div>
 </div>
+
+<style>
+	/* Icon grid
+   ========================================================================== */
+
+	.icon-grid {
+		display: grid;
+		gap: 1rem;
+
+		grid-template-columns: repeat(auto-fill, minmax(140px, 1fr));
+	}
+
+	.icon-grid li {
+		border: 1px solid #ccc;
+		padding: 1.5rem;
+		text-align: center;
+		border-radius: 3px;
+	}
+
+	@media (prefers-color-scheme: dark) {
+		.responds-to-dark .icon-grid li {
+			border-color: #555;
+		}
+	}
+
+	/* Icon item
+   ========================================================================== */
+
+	.icon-item {
+		position: relative;
+		min-height: 140px;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+	}
+
+	@media (prefers-color-scheme: dark) {
+		.responds-to-dark .icon-item {
+			color: #fff;
+		}
+
+		.responds-to-dark .icon-item :global(svg *) {
+			stroke: #fff;
+		}
+	}
+
+	.overlay {
+		display: none;
+	}
+
+	.icon-item:hover .overlay {
+		position: absolute;
+
+		width: 100%;
+		height: 100%;
+
+		background: rgba(85, 85, 85, 0.2);
+		backdrop-filter: blur(2px);
+
+		display: flex;
+		flex-direction: column;
+		justify-content: space-between;
+
+		padding: 8px;
+	}
+
+	.overlay .button {
+		cursor: pointer;
+		width: 100%;
+		padding: 12px;
+		font-size: 80%;
+	}
+</style>
