c3609f8ddf65fde9722d94399bd6a5756f69df33
Johan Ronsse <johan.ronsse@gmail.com>
2023-10-20 11:32:29 -0600

    feat - improve test page

--- packages/obra-icons-svelte/src/routes/test2/+page.svelte
+++ packages/obra-icons-svelte/src/routes/test2/+page.svelte
@@ -1,11 +1,42 @@
 <script>
 	import * as icons from '$package';
 	export let size = '24';
+	export let weight = 1.5;
+	export let color = '#FF0000';
 </script>
 
 <svelte:head>
 	<title>Test - Obra Icons</title>
 </svelte:head>
+
+<div class="controls">
+	<div class="control-group">
+		<label>Color</label><input type="color" bind:value={color} />
+		{color}
+	</div>
+
+	<div class="control-group">
+		<label>Size</label><input
+			type="range"
+			bind:value={size}
+			min="16"
+			step="4"
+			max="96"
+		/>
+		{size}
+	</div>
+
+	<div class="control-group">
+		<label>Weight</label><input
+			type="range"
+			bind:value={weight}
+			min=".5"
+			step=".5"
+			max="2"
+		/>
+		{weight}
+	</div>
+</div>
 
 <div class="padding-medium margin-0-auto">
 	<div class="vertical-container-xx-large">
@@ -14,9 +45,9 @@
 				<div class="icon-wrapper">
 					<svelte:component
 						this={component}
-						size={32}
-						color="red"
-						strokeWidth={1}
+						{size}
+						{color}
+						strokeWidth={weight}
 					/>
 				</div>
 			{/each}
@@ -28,5 +59,19 @@
 	.grid {
 		display: flex;
 		flex-wrap: wrap;
+		gap: 16px;
+	}
+
+	label {
+		font-weight: 600;
+	}
+
+	.controls {
+		display: flex;
+		padding: 16px;
+		gap: 24px;
+		background: red;
+		position: sticky;
+		top: 0;
 	}
 </style>
