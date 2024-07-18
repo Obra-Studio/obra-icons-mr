66a0055a99fc335c7b2f9da179f3e8a660b737eb
Johan Ronsse <johan.ronsse@gmail.com>
2024-07-18 14:28:51 -0600

    Fixes #21

--- packages/obra-icons-svelte/src/routes/icons/+page.svelte
+++ packages/obra-icons-svelte/src/routes/icons/+page.svelte
@@ -110,6 +110,6 @@
 	.icon-grid {
 		display: grid;
 		gap: 1rem;
-		grid-template-columns: repeat(auto-fill, minmax(140px, 1fr));
+		grid-template-columns: repeat(auto-fill, minmax(108px, 1fr));
 	}
 </style>
