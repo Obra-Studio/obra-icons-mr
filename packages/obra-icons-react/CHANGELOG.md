b0519140c9677b05b9085fc7a46e7a505c592f42
GHOST <ghostdevbusiness@gmail.com>
2024-09-16 03:48:11 +0100

    feat: react package groundwork

---
+++ packages/obra-icons-react/CHANGELOG.md
@@ -0,0 +1,7 @@
+# Changelog Obra Icons
+
+We use semantic versioning (semver).
+
+## 1.0
+
+Initial release
---
+++ packages/obra-icons-react/build.config.ts
@@ -0,0 +1,12 @@
+import { defineBuildConfig } from 'unbuild';
+
+export default defineBuildConfig({
+	declaration: true,
+	entries: [
+		{
+			builder: 'mkdist',
+			input: './src',
+			outDir: './dist',
+		},
+	],
+});
