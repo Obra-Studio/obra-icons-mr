f4f01356a0930c3321fd4d98a9384cbd2f2bba1a
Johan Ronsse <johan.ronsse@gmail.com>
2025-05-24 18:32:40 -0600

    Remove commercial info - invoicing info

--- packages/website/src/routes/support/+page.svelte
+++ packages/website/src/routes/support/+page.svelte
@@ -14,25 +14,6 @@
 					>support@obra.studio</a
 				>.
 			</p>
-
-			<h2>Invoicing & purchasing platform</h2>
-
-			<p>
-				When you purchase Obra Icons Files, the payment and invoicing is
-				handled through <a href="https://stripe.com/">Stripe</a>. The
-				digital goods will be delivered to you via
-				<a href="https://www.lemonsqueezy.com/">LemonSqueezy</a> upon payment.
-				Missing the delivery? Contact us!
-			</p>
-
-			<h2>Users & volume licensing</h2>
-			<p>
-				A license counts for one user. Need to support more users?
-				Simply purchase extra licenses. For volume licensing please
-				contact <a href="mailto:support@obra.studio"
-					>support@obra.studio</a
-				>.
-			</p>
 		</div>
 	</div>
 </div>
