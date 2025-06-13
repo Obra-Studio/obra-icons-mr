fac76c8cb665d86feb2aa233512c926e8b8f1171
Johan Ronsse <johan.ronsse@gmail.com>
2025-06-13 07:16:45 -0600

    Cross promote obra shadcn

---
+++ packages/website/src/lib/components/GlobalBanner.svelte
@@ -0,0 +1,21 @@
+<script>
+</script>
+
+<p>
+	With Obra, we just released a free, open source shadcn/ui kit. <a
+		href="https://obra.studio/blog/2025/06/12/obra-shadcn-ui-kit-officially-released/"
+		>Check out the announcement here</a
+	>.
+</p>
+
+<style>
+	p {
+		padding: 1rem;
+		text-align: center;
+	}
+
+	a {
+		color: #000;
+		font-weight: 700;
+	}
+</style>
--- packages/website/src/routes/+layout.svelte
+++ packages/website/src/routes/+layout.svelte
@@ -1,8 +1,11 @@
 <script lang="ts">
 	import Footer from '$lib/components/Footer.svelte';
 	import Header from '$lib/components/Header.svelte';
+	import GlobalBanner from '$lib/components/GlobalBanner.svelte';
 	import '$lib/screen.css';
 </script>
+
+<GlobalBanner />
 
 <Header />
 
