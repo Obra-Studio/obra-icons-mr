ad02aafaac0e25ab6b7bc1848f2f2d2ce11b97a6
Johan Ronsse <johan.ronsse@gmail.com>
2025-07-21 10:47:54 -0600

    Fixes #38

---
+++ packages/website/src/routes/+error.svelte
@@ -0,0 +1,12 @@
+<svelte:head>
+	<title>404 not found - Obra Icons</title>
+</svelte:head>
+
+<div class="vertical-container-xx-large">
+	<div class="container padding-x-large margin-0-auto text-align-center">
+		<div class="gap-medium flex flex-column">
+			<h2>404 not found</h2>
+			<a href="/">Go back home</a>
+		</div>
+	</div>
+</div>
